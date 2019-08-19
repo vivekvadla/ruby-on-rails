b04291e0c047ba4990669a825259d60beb868e54
vivekvadla <viveknani9t@gmail.com>
2019-08-19 11:07:49 +0530

    aug 19

--- tictactoe/app/controllers/players_controller.rb
+++ tictactoe/app/controllers/players_controller.rb
@@ -1,2 +1,5 @@
 class PlayersController < ApplicationController
+    def new
+        @player = Player.new
+      end
 end
---
+++ tictactoe2/app/controllers/players_controller.rb
@@ -0,0 +1,45 @@
+class PlayersController < ApplicationController
+    def index
+        @players = Player.order('lower(name)').all
+    end
+
+    def new
+        @player = Player.new
+    end
+
+    def create
+        @player = Player.new(player_params)
+        @players = Player.all
+        if @player.save
+            
+            redirect_to players_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @player = Player.find(params[:id])
+    end
+
+    def update
+        @player = Player.find(params[:id])
+        if params[:commit] == 'save'
+            @player.update(player_params)
+            redirect_to players_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @player = Player.find(params[:id])
+        @player.destroy
+        redirect_to players_path
+    end
+
+    private
+    def player_params
+        params.require(:player).permit(:name,:status,:commit)
+    end
+end
